e6e6307ad3b62f8eebaad35bc778aa8d8fa36905
uthem150 <uthem150@gmail.com>
2024-07-02 22:09:33 +0900

    mongoDB 연결

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require("express"); //express 모듈을 가져옴
 const app = express(); //새로운 express app생성
 const port = 5000;
 
-// 루트 디렉토리에 오면, 출력할 내용
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://ynmhobby:rHFLIhbz9wNINzVh@boiler-plate.eiv3gwi.mongodb.net/?retryWrites=true&w=majority&appName=boiler-plate",
+    {}
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
