ee89c81816938e253450bf74b28b6644a760b099
uthem150 <uthem150@gmail.com>
2024-07-06 21:49:10 +0900

    로그아웃 기능 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
+  const navigate = useNavigate();
+
   useEffect(() => {
     // get 요청을 서버에 보내고, 돌아오는 response를 console에 출력
     axios
@@ -14,6 +17,16 @@ function LandingPage() {
       });
   }, []);
 
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`).then((response) => {
+      if (response.data.success) {
+        navigate("/login");
+      } else {
+        alert("로그아웃에 실패했습니다");
+      }
+    });
+  };
+
   return (
     <div
       style={{
@@ -25,6 +38,8 @@ function LandingPage() {
       }}
     >
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
