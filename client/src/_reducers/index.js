2b41a7c16aa88914842dad13af460754d3ad5320
uthem150 <uthem150@gmail.com>
2024-07-06 19:01:54 +0900

    redux 적용

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,10 @@
+//여러 reducer들을 combineReducers를 이용해서 하나로 합쳐줌
+
+import { combineReducers } from "redux";
+//import user from "./user_reducer";
+
+const rootReducer = combineReducers({
+  //user,
+});
+
+export default rootReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,32 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import { Provider } from "react-redux";
 import "antd/dist/reset.css";
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./reducers";
+
+//createStore 만드는데, 객체 action만 받는 store가 promise, function과 받도록 미들웨어 적용
+const createProxyMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*  Redux 스토어를 React 애플리케이션에 적용 */}
+    <Provider
+      store={createProxyMiddleware(
+        Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
